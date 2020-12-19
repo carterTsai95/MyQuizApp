76d1dd50a1a011016c80f46ba67bf84c0fb3c759
Hung-Chun, Tsai <tsaihong1995@gmail.com>
2020-12-19 11:15:27 -0500

    The "View" and the connection between script has been finished

--- MyQuizApp/ViewController.swift
+++ MyQuizApp/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var quizImage: UIImageView!
+    
+    @IBOutlet weak var lblQuestion: UILabel!
+    
+    
+    let quiz = Quiz()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func trueBtnDidTouch(_ sender: Any) {
+    }
+    
+    @IBAction func falseBtnDidTouch(_ sender: Any) {
+    }
+    
 }
 
